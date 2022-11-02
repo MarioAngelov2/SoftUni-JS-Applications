9b796f8b5f318a9999416bd952b5301ce9786b1e
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-11-03 00:00:25 +0200

    Update app.js

--- Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
+++ Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
@@ -5,16 +5,21 @@ function solve() {
     const arriveBtn = document.getElementById('arrive');
 
     let stop = {
-        next: 'depot'
+        next: 'depott'
     };
 
     async function depart() {
         //get info for next stop
         //display next stop
-        
+
         departBtn.disabled = true;
         const url = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`;
         const response = await fetch(url);
+        if (response.status !== 200) {
+            label.textContent = 'Error';
+            departBtn.disabled = true;
+            arriveBtn.disabled = true;
+        }
         stop = await response.json();
 
         label.textContent = `Next Stop ${stop.name}`
