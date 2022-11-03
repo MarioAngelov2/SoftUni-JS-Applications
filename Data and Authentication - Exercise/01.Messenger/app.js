878c6f1dbebb39b74b9a5759e0a34a4b76579f53
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-11-03 23:01:15 +0200

    Added Exercise

--- Data and Authentication - Exercise/01.Messenger/app.js
+++ Data and Authentication - Exercise/01.Messenger/app.js
@@ -2,4 +2,25 @@ function attachEvents() {
     console.log('TODO...');
 }
 
+async function getAllMessage() {
+    const url = "http://localhost:3030/jsonstore/messenger";
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
+}
+
+async function createMessage(body) {
+    const url = "http://localhost:3030/jsonstore/messenger";
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    });
+    const data = await response.json();
+    return data;
+}
+
 attachEvents();
