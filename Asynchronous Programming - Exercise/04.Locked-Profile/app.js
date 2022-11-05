35c505250fb3ccbf34d00045e7660716551faef4
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-11-05 16:42:17 +0200

    .

--- Asynchronous Programming - Exercise/04.Locked-Profile/app.js
+++ Asynchronous Programming - Exercise/04.Locked-Profile/app.js
@@ -1,3 +1,11 @@
 function lockedProfile() {
     console.log('TODO...')
+
+   async function getUserInfo() {
+    const url = 'localhost:3030/jsonstore/advanced/profiles';
+    const response = await fetch(url);
+    const data = response.json();
+
+    return data;
+    }
 }
--- Data and Authentication - Exercise/02.Phonebook/app.js
+++ Data and Authentication - Exercise/02.Phonebook/app.js
@@ -2,4 +2,40 @@ function attachEvents() {
     console.log('TODO...');
 }
 
+async function onLoadAllRecords() {
+    const url = "http://localhost:3030/jsonstore/phonebook";
+    const response = await fetch(url);
+    const data = response.json();
+
+    return data;
+}
+
+async function onCreateRecord(person, phone) {
+    const url = "http://localhost:3030/jsonstore/phonebook";
+    body = {§
+        person,
+        phone
+    }
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "Application/json"
+        },
+        body: JSON.stringify(body)
+    })
+    const data = await response.json();
+    return data;
+}
+
+async function onDeleteRecord(id) {
+    const url = `http://localhost:3030/jsonstore/phonebook/${id}`;
+    const response = await fetch(url, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "Application/json"
+        },
+        body: JSON.stringify(body)
+    })
+}
+
 attachEvents();
