403d5a65eb448909fa8376185348c9b91245178b
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-11-07 21:20:28 +0200

    Update app.js

--- Asynchronous Programming - Exercise/03.Forecaster/app.js
+++ Asynchronous Programming - Exercise/03.Forecaster/app.js
@@ -3,7 +3,7 @@ function attachEvents() {
     document.getElementById('submit').addEventListener('click', getWeatherInfo);
 }
 
-async function getWeatherInfo() {
+async function getWeatherInfo() { 
     const url = 'http://localhost:3030/jsonstore/forecaster/locations';
     const response = await fetch(url);
     const data = response.json();
@@ -28,14 +28,38 @@ async function createForeCaster(code) {
 
     const responseThreeDays = await fetch(urlThreeDays);
     const dataThreeDays = await responseThreeDays.json();
+
+    createToday(dataToday);
 }
 
 function createToday(data) {
+    const {condition, high, low} = data.forecast;
     const conditionContainer = document.createElement('div');
     conditionContainer.classList.add('forecasts');
 
+    const conditionSymbol = document.createElement('span');
+    conditionSymbol.classList.add('condition');
+
     const conditionSpan = document.createElement('span');
-    conditionSpan.classList.add('forecast-data');
+    conditionSpan.classList.add('condition')
+
+    const nameSpan = document.createElement('span');
+    nameSpan.classList.add('forecast-data');
+    nameSpan.textContent = data.name;
+
+    const tempSpan = document.createElement('span');
+    tempSpan.classList.add('forecast-data');
+    tempSpan.textContent = `${data.low}/${data.high}`;
+
+    const contitionInfo = document.createElement('span');
+    contitionInfo.classList.add('forecast-data');
+    contitionInfo.textContent = data.condition; 
+
+    conditionSpan.appendChild(nameSpan);
+    conditionSpan.appendChild(tempSpan);
+    conditionSpan.appendChild(contitionInfo);
+    conditionContainer.appendChild(conditionSymbol);
+    conditionContainer.appendChild(conditionSpan);
 }
 
 attachEvents();
