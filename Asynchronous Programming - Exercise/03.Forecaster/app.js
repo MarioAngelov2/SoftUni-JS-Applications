df4a4ad5799827abbec8b413baa31055473eb3ea
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-11-08 20:43:16 +0200

    Update app.js

--- Asynchronous Programming - Exercise/03.Forecaster/app.js
+++ Asynchronous Programming - Exercise/03.Forecaster/app.js
@@ -1,3 +1,9 @@
+const generateIcons = {
+    "Sunny": "&#x2600",
+    "Partly sunny": "&#x26C5",
+    "Overcast": "&#x2601",
+    "Rain": "&#x2614"
+}
 
 function attachEvents() {
     document.getElementById('submit').addEventListener('click', getWeatherInfo);
@@ -5,21 +11,23 @@ function attachEvents() {
 
 async function getWeatherInfo() { 
     const url = 'http://localhost:3030/jsonstore/forecaster/locations';
-    const response = await fetch(url);
-    const data = response.json();
-    
-    const forecastConteiner = document.getElementById('forecast');
-    const currentDiv = document.getElementById('current');
-    const upcomingDiv = document.getElementById('upcoming');
-
     const townName = document.getElementById('location').value;
 
-    const info = Object.values(townName).find(x => x.name === townName);
+    const response = await fetch(url);
+    const data = await response.json();
     
+    // const forecastConteiner = document.getElementById('forecast');
+    // const currentDiv = document.getElementById('current');
+    // const upcomingDiv = document.getElementById('upcoming');
+
+    const info = data.find(x => x.name === townName);
     createForeCaster(info.code);
 }
 
 async function createForeCaster(code) {
+    const currentSection = document.getElementById('current');
+    const forecastContainer = document.getElementById('forecast');
+    const upcomingSection = document.getElementById('upcoming');
     const urlToday = `http://localhost:3030/jsonstore/forecaster/today/${code}`;
     const urlThreeDays = `http://localhost:3030/jsonstore/forecaster/upcoming/${code}`
 
@@ -28,8 +36,48 @@ async function createForeCaster(code) {
 
     const responseThreeDays = await fetch(urlThreeDays);
     const dataThreeDays = await responseThreeDays.json();
+    
+    forecastContainer.style.display = 'block'
+    const todayHtml = createToday(dataToday);
+    currentSection.appendChild(todayHtml);
 
-    createToday(dataToday);
+    const upcomingHtml = createUpcoming(dataThreeDays);
+    upcomingSection.appendChild(upcomingHtml)
+}
+
+function createUpcoming(data) {
+    const container = document.createElement('div');
+    container.classList.add('forecast-info');
+
+    data.forecast.forEach(data => {
+        const spanHolder = generateSpan(data);
+        container.appendChild(spanHolder)
+    })
+    return container
+}
+
+function generateSpan(data) {
+    const {condition, high, low} = data
+
+    const spanHolder = document.createElement('span');
+    spanHolder.classList.add('upcoming');
+
+    const iconSpan = document.createElement('span');
+    iconSpan.classList.add('symbol');
+    iconSpan.innerHTML = generateIcons[condition]
+
+    const degreeSpan = document.createElement('span');
+    degreeSpan.classList.add('forecast-data');
+    degreeSpan.innerHTML = `${low}&#176/${high}&#176`;
+
+    const conditionSpan = document.createElement('span');
+    conditionSpan.classList.add('forecast-data');
+    conditionSpan.innerHTML = `${condition}`;
+
+    spanHolder.appendChild(iconSpan);
+    spanHolder.appendChild(degreeSpan);
+    spanHolder.appendChild(conditionSpan);
+    return spanHolder
 }
 
 function createToday(data) {
@@ -38,7 +86,8 @@ function createToday(data) {
     conditionContainer.classList.add('forecasts');
 
     const conditionSymbol = document.createElement('span');
-    conditionSymbol.classList.add('condition');
+    conditionSymbol.classList.add('condition', 'symbol');
+    conditionSymbol.innerHTML = generateIcons[condition]
 
     const conditionSpan = document.createElement('span');
     conditionSpan.classList.add('condition')
@@ -49,17 +98,18 @@ function createToday(data) {
 
     const tempSpan = document.createElement('span');
     tempSpan.classList.add('forecast-data');
-    tempSpan.textContent = `${data.low}/${data.high}`;
+    tempSpan.innerHTML = `${low}&#176/${high}&#176`;
 
     const contitionInfo = document.createElement('span');
     contitionInfo.classList.add('forecast-data');
-    contitionInfo.textContent = data.condition; 
+    contitionInfo.textContent = `${condition}`; 
 
     conditionSpan.appendChild(nameSpan);
     conditionSpan.appendChild(tempSpan);
     conditionSpan.appendChild(contitionInfo);
     conditionContainer.appendChild(conditionSymbol);
     conditionContainer.appendChild(conditionSpan);
+    return conditionContainer;
 }
 
 attachEvents();
