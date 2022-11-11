d248ae9daeb5510f907168dbf56a83958a4148f0
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-11-11 17:42:14 +0200

    update

--- Data and Authentication - Exercise/01.Messenger/app.js
+++ Data and Authentication - Exercise/01.Messenger/app.js
@@ -1,46 +1,99 @@
 function attachEvents() {
-    document.getElementById('refresh').addEventListener('click', getAllMessage);
-    document.getElementById('submit').addEventListener('click', onSendMsg);
+    document.getElementById('refresh').addEventListener('click', getData);
+    document.getElementById('submit').addEventListener('click', onSendMsg)
 }
 
-function renderMsg(data) {
-  const textArea = document.getElementById('messages');
-  const response = Object.values(data).map(entry => `${entry.author}: ${entry.content}`).join('\n')
-  textArea.textContent = response
+function showInfo(data) {
+    const textArea = document.getElementById('messages');
+    debugger
+    const response = Object.values(data).map(entry => 
+        `${entry.author}: ${entry.content}`).join('\n');
+
+    textArea.textContent = response;
 }
 
 function onSendMsg() {
-    const author = document.querySelector("input[name='author']");
-    const content = document.querySelector("input[name='content']");
+    let nameInput = document.querySelector("input[name='author']");
+    let messageInput = document.querySelector("input[name='content']");
 
     const body = {
-        author: author.value,
-        content: content.value
+        author: nameInput.value,
+        content: messageInput.value
     }
-    author.value = '';
-    content.value = '';
-    createMessage(body)
+    nameInput.value = '';
+    messageInput.value = '';
+
+    createMessage(body);
 }
 
-async function getAllMessage() {
-    const url = "http://localhost:3030/jsonstore/messenger";
+async function getData() {
+    const url = 'http://localhost:3030/jsonstore/messenger';
     const response = await fetch(url);
     const data = await response.json();
-
-    renderMsg(data)
+    
+    showInfo(data)
 }
 
 async function createMessage(body) {
-    const url = "http://localhost:3030/jsonstore/messenger";
+    const url = 'http://localhost:3030/jsonstore/messenger';
     const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
+        method: "post",
+        headers: {"Content-Type": "application/json"},
         body: JSON.stringify(body)
     });
     const data = await response.json();
+
     return data;
 }
 
-attachEvents();
+attachEvents()
+
+
+
+
+// function attachEvents() {
+//     document.getElementById('refresh').addEventListener('click', getAllMessage);
+//     document.getElementById('submit').addEventListener('click', onSendMsg);
+// }
+
+// function renderMsg(data) {
+//   const textArea = document.getElementById('messages');
+//   const response = Object.values(data).map(entry => `${entry.author}: ${entry.content}`).join('\n')
+//   textArea.textContent = response
+// }
+
+// function onSendMsg() {
+//     const author = document.querySelector("input[name='author']");
+//     const content = document.querySelector("input[name='content']");
+
+//     const body = {
+//         author: author.value,
+//         content: content.value
+//     }
+//     author.value = '';
+//     content.value = '';
+//     createMessage(body)
+// }
+
+// async function getAllMessage() {
+//     const url = "http://localhost:3030/jsonstore/messenger";
+//     const response = await fetch(url);
+//     const data = await response.json();
+
+//     renderMsg(data)
+// }
+
+// async function createMessage(body) {
+//     const url = "http://localhost:3030/jsonstore/messenger";
+//     const response = await fetch(url, {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(body)
+//     });
+//     const data = await response.json();
+//     return data;
+// }
+
+// attachEvents();
--- Data and Authentication - Exercise/02.Phonebook/app.js
+++ Data and Authentication - Exercise/02.Phonebook/app.js
@@ -1,41 +1,155 @@
+const ul = document.getElementById('phonebook');
+
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('btnLoad').addEventListener('click', getData);
+    document.getElementById('btnCreate').addEventListener('click', onCreate);
 }
 
-async function onLoadAllRecords() {
-    const url = "http://localhost:3030/jsonstore/phonebook";
-    const response = await fetch(url);
-    const data = response.json();
+function onLoad(data) {
 
-    return data;
+    Object.values(data).forEach(el => {
+        const delBtn = document.createElement('button');
+        delBtn.textContent = 'Delete';
+        const li = document.createElement('li');
+        li.textContent = `${el.person}: ${el.phone}`;
+        li.setAttribute('data-id', el._id);
+        li.appendChild(delBtn);
+        ul.appendChild(li);
+    });
+
 }
 
-async function onCreateRecord(person, phone) {
-    const url = "http://localhost:3030/jsonstore/phonebook";
-    body = {§
-        person,
-        phone
+function onCreate() {
+    const personInput = document.getElementById('person');
+    const phoneInput = document.getElementById('phone');
+
+    const body = {
+        person: personInput.value,
+        phone: phoneInput.value
     }
+
+    postData(body);
+}
+
+async function getData() {
+    const url = 'http://localhost:3030/jsonstore/phonebook';
+    const response = await fetch(url);
+    const data = await response.json();
+
+   return onLoad(data);
+}
+
+async function postData(body) {
+    const url = 'http://localhost:3030/jsonstore/phonebook';
     const response = await fetch(url, {
-        method: "POST",
+        method: 'post',
         headers: {
-            "Content-Type": "Application/json"
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(body)
-    })
+
+    });
     const data = await response.json();
+
     return data;
 }
 
-async function onDeleteRecord(id) {
+async function deleteData(id) {
     const url = `http://localhost:3030/jsonstore/phonebook/${id}`;
     const response = await fetch(url, {
-        method: "DELETE",
+        method: 'delete',
         headers: {
-            "Content-Type": "Application/json"
+            "Content-Type": "application/json"
         },
-        body: JSON.stringify(body)
-    })
+        body: JSON.stringify(null)
+    });
+    const data = response.json();
+    return data;
 }
 
-attachEvents();
+attachEvents()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function attachEvents() {
+//     console.log('TODO...');
+// }
+
+// // async function onLoadAllRecords() {
+// //     const url = "http://localhost:3030/jsonstore/phonebook";
+// //     const response = await fetch(url);
+// //     const data = response.json();
+
+// //     return data;
+// // }
+
+// // async function onCreateRecord(person, phone) {
+// //     const url = "http://localhost:3030/jsonstore/phonebook";
+// //     body = {
+// //         person,
+// //         phone
+// //     }
+// //     const response = await fetch(url, {
+// //         method: "POST",
+// //         headers: {
+// //             "Content-Type": "Application/json"
+// //         },
+// //         body: JSON.stringify(body)
+// //     })
+// //     const data = await response.json();
+// //     return data;
+// // }
+
+// // async function onDeleteRecord(id) {
+// //     const url = `http://localhost:3030/jsonstore/phonebook/${id}`;
+// //     const response = await fetch(url, {
+// //         method: "DELETE",
+// //         headers: {
+// //             "Content-Type": "Application/json"
+// //         },
+// //         body: JSON.stringify(body)
+// //     })
+// // }
+
+// attachEvents();
