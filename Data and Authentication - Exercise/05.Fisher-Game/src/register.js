93f0aa7941cf21f45ff7a7ef0d736eb93a739cca
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-11-13 00:00:09 +0200

    update

--- Data and Authentication - Exercise/05.Fisher-Game/src/register.js
+++ Data and Authentication - Exercise/05.Fisher-Game/src/register.js
@@ -1,1 +1,62 @@
-console.log('TODO:// Implement Register functionality');
+document.getElementById('register-form').addEventListener('submit', registerHandler);
+document.querySelectorAll('a').forEach(x => x.classList.remove('active'));
+document.getElementById('register').classList.add('active');
+const errorP = document.querySelector('p.notification');
+
+function registerHandler(ev) {
+    // validation
+    ev.preventDefault();
+
+    const form = ev.target;
+    const formData = new FormData(form);
+    const { email, password, rePassword } = Object.fromEntries(formData);
+
+    if (password !== rePassword) {
+        setTimeout(() => {
+            errorP.textContent = 'Error!';
+        }, 1000)
+    }
+
+    onRegister(email, password);
+}
+
+
+async function onRegister(email, password) {
+
+    //error handiling
+    const url = 'http://localhost:3030/users/register';
+    const body = {
+        email,
+        password
+    }
+    const header = getHeader('POST', body);
+    try {
+        const response = await fetch(url, header);
+        const data = await response.json();
+        if (data.code !== 200) {
+            throw new Error(data.message)
+        }
+
+        sessionStorage.setItem('email', data.email);
+        sessionStorage.setItem('accessToken', data.accessToken)
+        window.location = './index.html';
+        
+        return data
+    } catch (error) {
+        errorP.textContent = error;
+        setTimeout(() => {
+            errorP.textContent = '';
+        }, 2000)
+    }
+
+}
+
+function getHeader(method, body) {
+    return {
+        method: `${method}`,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    }
+}
