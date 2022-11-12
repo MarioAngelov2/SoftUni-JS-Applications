f1d7ee8a25d015ac81887abf4b57686b65f39d2b
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-11-12 16:06:04 +0200

    update

--- Data and Authentication - Exercise/03.Students/app.js
+++ Data and Authentication - Exercise/03.Students/app.js
@@ -1,1 +1,65 @@
-console.log('TODO...');
+async function getData() {
+    const table = document.querySelector('#results tbody');
+    document.getElementById('submit').addEventListener('click', onSubmit)
+
+    const firstNameInput = document.querySelector('input[name="firstName"]');
+    const lastNameInput = document.querySelector('input[name="lastName"]');
+    const facultyNumberInput = document.querySelector('input[name="facultyNumber"]');
+    const gradeInput = document.querySelector('input[name="grade"]');
+
+    const url = 'http://localhost:3030/jsonstore/collections/students';
+    const response = await fetch(url);
+    const data = await response.json();
+
+    Object.values(data).forEach(s => {
+        const firstName = s.firstName;
+        const lastName = s.lastName;
+        const fcNumber = s.facultyNumber;
+        const grade = Number(s.grade);
+
+        const tr = document.createElement('tr');
+
+        const firstNameCell = tr.insertCell(0);
+        firstNameCell.textContent = firstName;
+
+        const lastNameCell = tr.insertCell(1);
+        lastNameCell.textContent = lastName;
+
+        const fcNumberCell = tr.insertCell(2);
+        fcNumberCell.textContent = fcNumber;
+
+        const gradeCell = tr.insertCell(3);
+        gradeCell.textContent = grade;
+
+
+        
+        table.appendChild(tr)
+    });
+
+    function onSubmit(ev) {
+        ev.preventDefault();
+
+        const tr = document.createElement('tr');
+
+        const firstNameCell = tr.insertCell(0);
+        firstNameCell.textContent = firstNameInput.value;
+
+        const lastNameCell = tr.insertCell(1);
+        lastNameCell.textContent = lastNameInput.value;
+
+        const fcNumberCell = tr.insertCell(2);
+        fcNumberCell.textContent = facultyNumberInput.value;
+
+        const gradeCell = tr.insertCell(3);
+        gradeCell.textContent = gradeInput.value;
+
+        firstNameInput.value = '';
+        lastNameInput.value = '';
+        facultyNumberInput.value = '';
+        gradeInput.value = '';
+
+        table.appendChild(tr)
+    }
+}
+getData()
+
