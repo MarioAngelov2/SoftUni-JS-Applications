85169b492c61d65b5d6885219ef4c71227632280
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-11-13 23:58:32 +0200

    update

---
+++ Architecture and Testing - Exercise/03.SoftTerest/app.js
@@ -0,0 +1,8 @@
+
+const homeView = document.getElementById('homeView')
+const registerView = document.getElementById('registerView')
+const dashboard = document.getElementById('dashboard-holder')
+const detailsView = document.getElementById('homeView')
+const createView = document.getElementById('createView')
+
+const defSection = document.getElementById('defSection').remove();
--- Single Page Applications - Exercise/01.Forum/src/details.js
+++ Single Page Applications - Exercise/01.Forum/src/details.js
@@ -1,6 +1,8 @@
 const section = document.getElementById('detailsView');
 const main = document.getElementsByTagName('main')[0];
 const form = section.querySelector('form');
+form.addEventListener('submit', onSubmit);
+
 let id;
 section.remove();
 
@@ -12,32 +14,41 @@ export async function showDetails(ev) {
         id = ev.target.parentElement.id
     } else if (ev.target.tagName == 'A') {
         id = ev.target.id
-    } 
-    const topic = loadTopic(id)
-    //const comments = loadComment(id);
-    //const res = topicTemplate(topic, comments);
+    }
+
+    const topic = await loadTopic(id)
+    const comments = await loadComment(id)
+
+    const res = topicTemplate(topic, comments);
     //section.replaceChildren(res)
 
     main.replaceChildren(section);
 }
 
-function topicTemplate(data) {
-
+function topicTemplate(topic, comments) {
+    const topicContainer = document.createElement('div');
+    topicContainer.classList.add('theme-title');
+    topicContainer.innerHTML = `
+                        <div class="theme-name-wrapper">
+                            <div class="theme-name">
+                                <h2>${topic.topicName}</h2>
+                            </div>
+                        </div>`
 }
 
 function onSubmit(ev) {
     ev.preventDefault();
 
     const formData = new FormData(form);
-    const {postText, username} = Object.fromEntries(formData);
-    createPost({postText, username, id});
+    const { postText, username } = Object.fromEntries(formData);
+    createPost({ postText, username, id });
 }
 
 async function createPost(body) {
     const url = 'http://localhost:3030/jsonstore/collections/myboard/comments';
     const response = await fetch(url, {
         method: 'POST',
-        headers: {"Content-Type": "application/json"},
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body)
     })
     const data = await response.json();
@@ -46,6 +57,13 @@ async function createPost(body) {
 
 function clearForm() {
     form.reset();
+}
+
+async function loadComment(id) {
+    const url = 'http://localhost:3030/jsonstore/collections/myboard/comments' // id?
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
 }
 
 async function loadTopic(id) {
