e22b9f53f5ea5ac9262aec8a86fba34697141d8a
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-11-15 22:11:11 +0200

    Update search.js

--- Client Side Rendering - Exercise/03.Search-in-List/search.js
+++ Client Side Rendering - Exercise/03.Search-in-List/search.js
@@ -1,3 +1,35 @@
-function search() {
-   // TODO
+import { towns } from './towns.js';
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+
+const townsSection = document.getElementById('towns');
+const searchField = document.getElementById('searchText');
+const searchBtn = document.querySelector('button');
+searchBtn.addEventListener('click', search)
+
+update()
+
+function search(towns) {
+   console.log(townTemplate)
+}
+
+function searchTemplate(townsName) {
+   const ul = html`
+   <ul>
+      ${townsName.map(townsName => createLiTemplate(townsName))}
+   </ul>
+`
+return ul;
+}
+
+function createLiTemplate(town) {
+   return html`
+      <li>
+         ${town}
+      </li>
+   `
+}
+
+function update() {
+   const ul = searchTemplate(towns);
+   render(ul, townsSection)
 }
