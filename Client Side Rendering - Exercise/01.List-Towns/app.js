3d5fed86926693934826ecb79660cc89df8b8cc7
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-11-15 20:30:37 +0200

    Update app.js

--- Client Side Rendering - Exercise/01.List-Towns/app.js
+++ Client Side Rendering - Exercise/01.List-Towns/app.js
@@ -1,11 +1,31 @@
 import {html, render} from '../node_modules/lit-html/lit-html.js';
 
-const form = document.querySelector('form')
-form.addEventListener('submit', onSubmit)
+const form = document.querySelector('form');
+form.addEventListener('submit', onSubmit);
+
+const root = document.getElementById('root');
 
 function onSubmit(ev) {
     ev.preventDefault(); 
 
     const formData = new FormData(form);
-    const {towns} = Object.fromEntries(formData) 
+    const {towns} = Object.fromEntries(formData);
+    const arrTowns = towns.split(', ');
+    renderData(arrTowns);
+
+    form.reset();
+}
+
+function renderData(data) {
+    const result = createTownList(data);
+    render(result, root);
+}
+
+function createTownList(data) {
+    const ul = html `
+        <ul>
+            ${data.map(el => html `<li>${el}</li>`)}
+        </ul>
+    `
+    return ul;
 }
