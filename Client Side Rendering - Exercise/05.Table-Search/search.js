81a519d3880d1051074fc564916197454086f7c9
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-11-16 21:07:00 +0200

    Create search.js

---
+++ Client Side Rendering - Exercise/05.Table-Search/search.js
@@ -0,0 +1,15 @@
+export function solve() {
+    const tRows = document.querySelector('.container tbody').children();
+    const inputField = document.getElementById('searchField');
+    let match = inputField.value;
+    inputField.value = '';
+
+    for (let row of tRows) {
+
+        row.classList.remove('select');
+
+        if (row.textContent.toLowerCase().includes(match)){
+            row.classList.add('select');
+        }
+    }
+}
