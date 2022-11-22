d1f190741e54c348b2bf39c69a5155aee2ca0f81
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2022-11-23 00:31:34 +0200

    update

--- Exam Prep/Pet Care/src/app.js
+++ Exam Prep/Pet Care/src/app.js
@@ -1,17 +1,20 @@
 import { html ,render } from '../node_modules/lit-html/lit-html.js';
 import page from '../node_modules/page/page.mjs';
+import { showCatalog } from './views/catalog.js';
 import { showHome } from './views/home.js';
+import { showLogin } from './views/login.js';
+import { showRegister } from './views/register.js';
 
 const main = document.getElementById('content');
 
 page(decorateContext);
 page('/', showHome);
-page('/catalog', () => console.log('catalog'));
+page('/catalog', showCatalog);
 page('/catalog/:id', () => console.log('details'));
 page('/edit/:id', () => console.log('edit'));
 page('/create', () => console.log('create'));
-page('/login', () => console.log('login'));
-page('/register', () => console.log('register'));
+page('/login', showLogin);
+page('/register', showRegister);
 
 page.start();
 
